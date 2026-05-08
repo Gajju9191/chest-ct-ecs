863eb1b1238b3b679678f83820829951d84dff5f
Gajju9191 <gajananw9191@gmail.com>
2026-05-08 18:32:21 +0530

    prepare model updated

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 
 # main.py
+# main.py
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
 
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n==========x==========")
-except Exception as e:
-    logger.exception(e)
-    raise e
+if __name__ == "__main__":
+    # Stage 1: Data Ingestion
+    try:
+        logger.info(">>>>>> stage Data Ingestion Stage started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(">>>>>> stage Data Ingestion Stage completed <<<<<<\n\n==========x==========")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+    
+    # Stage 2: Prepare Base Model
+    try:
+        logger.info(">>>>>> stage Prepare Base Model Stage started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(">>>>>> stage Prepare Base Model Stage completed <<<<<<\n\n==========x==========")
+    except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,38 @@
+
+# src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier import logger
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        try:
+            logger.info(f"Starting {STAGE_NAME}")
+            
+            config = ConfigurationManager()
+            prepare_base_model_config = config.get_prepare_base_model_config()
+            prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+            prepare_base_model.get_base_model()
+            prepare_base_model.update_base_model()
+            
+            logger.info(f"{STAGE_NAME} completed successfully!")
+            
+        except Exception as e:
+            logger.error(f"{STAGE_NAME} failed: {e}")
+            raise e
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n==========x==========")
+    except Exception as e:
+        logger.exception(e)
+        raise e
