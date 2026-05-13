16df3586e9bf6e91a03df984acb984e858b2ab25
Gajju9191 <gajananw9191@gmail.com>
2026-05-13 13:47:11 +0530

    jenkinsfile added

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
-
 FROM python:3.11-slim
 
-RUN apt-get update -y && apt-get install awscli -y
+RUN apt-get update -y && apt-get install -y awscli
 WORKDIR /app
 
-COPY . /app
-RUN pip install -r requirements.txt
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 
-CMD ["python3", "app.py"]
+# Copy the rest of the application (model.h5 will be downloaded by Jenkins)
+COPY . .
+
+EXPOSE 8080
+
+CMD ["python", "app.py"]
---
+++ scripts/jenkins.sh
@@ -0,0 +1,54 @@
+
+#!bin/bash
+
+sudo apt update 
+
+sudo apt install openjdk-8-jdk -y
+
+https://pkg.jenkins.io/
+https://pkg.jenkins.io/debian-stable/
+
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins
+
+sudo systemctl status jenkins
+
+
+
+## Installing Docker
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+sudo usermod -aG docker $USER
+
+sudo usermod -aG docker jenkins
+
+
+newgrp docker
+
+sudo apt install awscli -y
+
+if aws cli has  no installation candidate then run below command (it gets error because latest ubuntu version doesn't support directly to aws cli)
+
+sudo apt install unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install && rm -rf awscliv2.zip aws/ && aws --version
+
+sudo usermod -a -G docker jenkins
+
+
+## AWS configuration & restarts jenkins
+
+aws configure
+
+
+## Now setup elastic IP on AWS
+
+
+
+## For getting the admin password for jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
